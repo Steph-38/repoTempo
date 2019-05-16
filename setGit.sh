9e9dcc1c61f0587b482fa3723963e9471ead4624
admin <admin@iMac3.local>
2019-05-16 13:58:36 +0200

    Suppression des fichiers en trop

--- setGit.sh
+++ setGit.sh
@@ -1,8 +1,17 @@
+# Initialisation du repository local
 git init
+
+# Ajout des fichiers et dossiers
 git add --all
+
+# commit de l'ajout
 git commit -m "first commit"
 clear
+
+# saisie de l'url pour ajouter comme remote
 echo "Insérer l'URL du Repo ici :"
 read theUrl
 git remote add origin $theUrl
+
+# Push du repository local vers le repository distant
 git push -u origin master
